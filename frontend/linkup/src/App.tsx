5ea3f6343ae46c86013a7aca4eef1f7ea6d23dcf
AndyAtmadja03S <atmandy345@gmail.com>
2025-03-24 15:50:14 +1100

    done with login

---
+++ frontend/linkup/src/App.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { FC, ReactNode } from "react";
+import Cookies from "js-cookie"
+
+import Login from "./pages/Login";
+
+// const ProtectedRoute: FC<{children?: ReactNode}> = ({ children }) => {
+//   const token = Cookies.get("userinfo"); // Retrieve the token cookie
+
+//   // If token doesn't exist, redirect to the login page
+//   if (!token) {
+//     return <Navigate to="/login" replace />;
+//   }
+//   // Render the children (protected content) if authenticated
+//   return children;
+// };
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            {/* If not logged in already, redirect to login, otherwise redirect to my calendars.*/}
+            
+              <Route path="/" element={<Login />}></Route>
+              {/* <Route path="/home" element={<Login />}></Route>
+              <Route path="/market" element={<ProtectedRoute><MyCalendars /></ProtectedRoute>}></Route>
+              <Route path="/connect" element={<ProtectedRoute><Calendar /></ProtectedRoute>}></Route>
+              <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>}></Route>
+              <Route path="/messages" element={<Help />}></Route> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
